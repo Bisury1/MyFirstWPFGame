4a9612eb737c41c57723ac416e39bd8f30a56cb9
Bisury1 <fnebaev@mail.ru>
2022-07-29 14:37:49 +0300

    first commit

---
+++ MyGame/ApplicationViewModel.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net.Mime;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Xml.Serialization;
+using MyGame.Annotations;
+
+namespace MyGame
+{
+    internal class ApplicationViewModel : INotifyPropertyChanged
+    {
+        private int _score;
+        private List<HighScoreForSerialize> _highScore = new List<HighScoreForSerialize>();
+        private ObservableCollection<ObservableCollection<Cell>> _gameField;
+        private Game _game;
+        private string _userName;
+        private bool _gameIsOver;
+        private bool _gameIsRunning;
+        private ICommand _moveCommand;
+        private ICommand _startCommand;
+        private ICommand _showRecord;
+        private string _textForTextBox;
+        private readonly string _path = ConfigurationSettings.AppSettings["RecordFile"];
+        public int CurrentScore
+        {
+            get => _score;
+            set
+            {
+                _score = value;
+                if (_userName != null)
+                {
+                    if (_score > ReturnCurrentUser().HighScore)
+                    {
+                        HighScore = _score;
+                    }
+                }
+
+                OnPropertyChanged();
+            }
+        }
+
+        private HighScoreForSerialize ReturnCurrentUser()
+        {
+
+            var item = _highScore.Find(x => _userName == x.userName);
+            if (item == null)
+            {
+                item = new HighScoreForSerialize(_userName);
+                _highScore.Add(item);
+            }
+            return item;
+        }
+
+        public int HighScore
+        {
+            get
+            {
+                if (_userName != null)
+                {
+                    var user = ReturnCurrentUser();
+                    user = ReturnCurrentUser();
+                    return user.HighScore;
+                }
+
+                return 0;
+            }
+            set
+            {
+                if (_userName != null)
+                {
+                    var user = ReturnCurrentUser();
+                    user.HighScore = value;
+                }
+
+                OnPropertyChanged();
+            }
+        }
+
+        public ObservableCollection<ObservableCollection<Cell>> Field
+        {
+            get => _gameField;
+            set
+            {
+                _gameField = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool GameOver
+        {
+            get => _gameIsOver;
+            set
+            {
+                _gameIsOver = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string UserName
+        {
+            get => _userName;
+            set
+            {
+                _userName = value;
+                OnPropertyChanged();
+            }
+        }
+        public bool GameRunning
+        {
+            get => _gameIsRunning;
+            set
+            {
+                _gameIsRunning = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public void EndGame()
+        {
+            TextForTextBox = "";
+            GameRunning = false;
+            GameOver = true;
+            if (ReturnCurrentUser().HighScore < CurrentScore)
+            {
+                var user = ReturnCurrentUser();
+                user.HighScore = CurrentScore;
+            }
+            var bs = new XmlSerializer(typeof(List<HighScoreForSerialize>));
+            using (Stream sw = new FileStream(_path, FileMode.Create, FileAccess.Write))
+            {
+                bs.Serialize(sw, _highScore);
+            }
+        }
+
+        private void NewGame()
+        {
+            if (GameRunning)
+                _game.Stop();
+            var bs = new XmlSerializer(typeof(List<HighScoreForSerialize>));
+            try
+            {
+                using (Stream sw = new FileStream(_path, FileMode.Open, FileAccess.Read))
+                {
+                    if (sw.Length > 0)
+                    {
+                        _highScore = (List<HighScoreForSerialize>)bs.Deserialize(sw);
+                    }
+                }
+            }
+            catch (IOException)
+            {
+            }
+            GameOver = false;
+            CurrentScore = 0;
+            _game = new Game(this);
+            Field = _game.GameField;
+        }
+
+        public ApplicationViewModel()
+        {
+            NewGame();
+        }
+
+        public ICommand StartCommand
+        {
+            get
+            {
+                return _startCommand ?? (_startCommand = new RelayCommand(parameter =>
+                {
+                    if (!string.IsNullOrEmpty(_userName))
+                    {
+                        if (!GameRunning)
+                        {
+                            if (GameOver)
+                            {
+                                HighScore = ReturnCurrentUser().HighScore;
+                                NewGame();
+                            }
+                            else
+                            {
+                                GameRunning = true;
+                                HighScore = ReturnCurrentUser().HighScore;
+                                _game.Start();
+                            }
+                        }
+                        else
+                        {
+                            GameRunning = false;
+                            _game.Stop();
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Сначала введите имя");
+                    }
+                }));
+            }
+        }
+
+        public string TextForTextBox
+        {
+            get => _textForTextBox;
+            set
+            {
+                _textForTextBox = value;
+                OnPropertyChanged();
+            }
+        }
+        public ICommand ShowRecord
+        {
+            get
+            {
+                return _showRecord ?? (_showRecord = new RelayCommand(parameter =>
+                {
+                    TextForTextBox = "";
+                    var sb = new StringBuilder();
+                    foreach (var item in _highScore)
+                    {
+                        sb.Append($"{item.userName} {item.HighScore}\n");
+                    }
+                    TextForTextBox = sb.ToString();
+                }));
+            }
+        }
+
+        public ICommand MoveCommand
+        {
+            get
+            {
+                return _moveCommand ?? (_moveCommand = new RelayCommand(parameter =>
+                {
+                    if (GameRunning && Enum.TryParse(parameter.ToString(), out Direction direction))
+                    {
+                        _game.direction = direction;
+                    }
+                }));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ MyGame/ClassesForGame.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace MyGame
+{
+    #region RelayCommand
+    internal class RelayCommand: ICommand
+    {
+        private Action<object> execute;
+        private Func<object, bool> canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return this.canExecute == null || this.canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            this.execute(parameter);
+        }
+    }
+    #endregion
+
+    #region EnumeratorForGame
+
+    internal enum Direction
+    {
+        Right,
+        Up,
+        Left,
+        Down
+    }
+
+    internal enum TypeOfCell
+    {
+        Empty,
+        Food,
+        Snake
+    }
+
+    internal struct Coord
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Coord(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+
+    #endregion
+    #region ClassesForMyGame
+    [Serializable]
+    public class HighScoreForSerialize
+    {
+        public int HighScore { get; set; }
+        public string userName { get; set; }
+
+        public HighScoreForSerialize(string user)
+        {
+            userName = user;
+        }
+
+        public HighScoreForSerialize()
+        {
+        }
+    }
+    internal class Cell: INotifyPropertyChanged
+    {
+        private TypeOfCell _type;
+
+        public TypeOfCell typeOfCell
+        {
+            get => _type;
+            set
+            {
+                _type = value;
+                OnPropertyChanged();
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+    internal class Snake
+    {
+        private ObservableCollection<ObservableCollection<Cell>> _gameField;
+        private Queue<Coord> _snakeBody = new Queue<Coord>();
+        private Game _game;
+        private Coord _snakeHead;
+        private int _length;
+        public bool IsDied { get; set; }
+
+        public Snake()
+        {
+        }
+        public Snake(ObservableCollection<ObservableCollection<Cell>> gameField, Game game)
+        {
+            _game = game;
+            _gameField = gameField; 
+            SnakeHead = new Coord(gameField[0].Count / 2, gameField.Count / 2);
+            _length = 0;
+        }
+
+        private int ModField(int number, int count)
+        {
+            if (number < 0)
+            {
+                return number + count;
+            }
+
+            if (number >= count)
+            {
+                return number % count;
+            }
+
+            return number;
+        }
+        public void Move(Direction direction)
+        {
+            Coord nextCoord = new Coord();
+            switch (direction)
+            {
+                case Direction.Right:
+                    nextCoord = new Coord(ModField((_snakeHead.X + 1),_gameField.Count), ModField((_snakeHead.Y),_gameField[0].Count));
+                    break;
+                case Direction.Up:
+                    nextCoord = new Coord(ModField((_snakeHead.X), _gameField.Count), ModField((_snakeHead.Y - 1), _gameField[0].Count));
+                    break;
+                case Direction.Left:
+                    nextCoord = new Coord(ModField((_snakeHead.X - 1), _gameField.Count), ModField((_snakeHead.Y), _gameField[0].Count));
+                    break;
+                case Direction.Down:
+                    nextCoord = new Coord(ModField((_snakeHead.X), _gameField.Count), ModField((_snakeHead.Y + 1), _gameField[0].Count));
+                    break;
+            }
+            if (!CheckMove(nextCoord))
+                return;
+            _snakeBody.Enqueue(_snakeHead);
+            SnakeHead = nextCoord;
+            if (_length < _snakeBody.Count)
+            {
+                var delCell = _snakeBody.Dequeue();
+                _gameField[delCell.X][delCell.Y].typeOfCell = TypeOfCell.Empty;
+            }
+        }
+
+        public bool CheckMove(Coord coord)
+        {
+            if (_snakeBody.Contains(coord))
+            {
+                IsDied = true;
+                return false;
+            }
+            if(_gameField[coord.X][coord.Y].typeOfCell == TypeOfCell.Food)
+            {
+                _gameField[coord.X][coord.Y].typeOfCell = TypeOfCell.Empty;
+                _game.AddFood();
+                _length++;
+            }
+
+            return true;
+        }
+
+        public Coord SnakeHead
+        {
+            get => _snakeHead;
+            set
+            {
+                _snakeHead = value;
+                _gameField[value.X][value.Y].typeOfCell = TypeOfCell.Snake;
+            }
+        }
+    }
+
+    internal class Game
+    {
+        private ApplicationViewModel _viewModel;
+        private Snake _snake;
+        private Direction _direction;
+        private CancellationTokenSource _cts = null;
+        private short _timer = 200;
+        private bool _delayAfterChangeDirection;
+        Random rnd = new Random();
+        public ObservableCollection<ObservableCollection<Cell>> GameField { get; set; }
+
+        public Game(ApplicationViewModel viewModel)
+        {
+            _viewModel = viewModel;
+            GameField = new ObservableCollection<ObservableCollection<Cell>>();
+            for (int i = 0; i < 29; i++)
+            {
+                GameField.Add(new ObservableCollection<Cell>());
+                for (int j = 0; j < 27; j++)
+                {
+                    GameField[i].Add(new Cell());
+                }
+            }
+
+            GameField[15][12].typeOfCell = TypeOfCell.Food;
+            _snake = new Snake(GameField, this);
+        }
+
+        public void Stop()
+        {
+            _cts?.Cancel();
+        }
+
+        public void Start()
+        {
+            if (_cts == null)
+                Run();
+        }
+
+        private async void Run()
+        {
+            using (_cts = new CancellationTokenSource())
+            {
+                try
+                {
+                    while (true)
+                    {
+                        if (_snake.IsDied)
+                        {
+                            _viewModel.EndGame();
+                            break;
+                        }
+                        Update();
+                        await Task.Delay(_timer, _cts.Token);
+                        if (_delayAfterChangeDirection)
+                        {
+                            _delayAfterChangeDirection = false;
+                            await Task.Delay(_timer / 3, _cts.Token);
+                        }
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+
+            _cts = null;
+
+        }
+
+        public void Update()
+        {
+            _snake.Move(direction);
+        }
+
+        public void AddFood()
+        {
+            while (true)
+            {
+                var coord = new Coord(rnd.Next(0, GameField.Count), rnd.Next(GameField[0].Count));
+                if (GameField[coord.X][coord.Y].typeOfCell != TypeOfCell.Empty) continue;
+                GameField[coord.X][coord.Y].typeOfCell = TypeOfCell.Food;
+                _viewModel.CurrentScore += 20;
+                break;
+            }
+        }
+
+        public Direction direction
+        {
+            get => _direction;
+            set
+            {
+                if ((int) value % 2 != (int) _direction % 2 && value != _direction)
+                {
+                    _direction = value;
+                    _delayAfterChangeDirection = true;
+                    Update();
+                }
+            }
+        }
+    }
+
+
+    #endregion
+}
